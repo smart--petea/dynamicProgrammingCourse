e444c4bbf53ff70f13e5b70b5900f9a21e06e543
Petru <smart__petea@mail.ru>
2021-01-17 21:08:42 +0200

    2:30:05 can construct memo

--- can_construct.go
+++ can_construct.go
@@ -5,18 +5,30 @@ import (
 )
 
 func CanConstruct(target string, wordBank []string) bool {
+	memo := make(map[string]bool)
+	return canConstruct(target, wordBank, memo)
+}
+
+func canConstruct(target string, wordBank []string, memo map[string]bool) bool {
 	if len(target) == 0 {
 		return true
 	}
 
+	if val, ok := memo[target]; ok {
+		return val
+	}
+
+
+	result := false
 	for _, prefix := range wordBank {
 		if strings.HasPrefix(target, prefix) {
 			targetWithoutPrefix := target[len(prefix):]
-			if CanConstruct(targetWithoutPrefix, wordBank) {
-				return true
+			if canConstruct(targetWithoutPrefix, wordBank, memo) {
+				result = true
 			}
 		}
 	}
 
-	return false
+	memo[target] = result
+	return result
 }
